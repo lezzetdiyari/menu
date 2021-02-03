65f92bed0b6242c8bbd0496e9fee2e24456c95bf
aks04-bit <76591721+aks04-bit@users.noreply.github.com>
2021-02-03 12:37:12 +0300

    yrkfhngb

---
+++ script/main.js
@@ -0,0 +1,13 @@
+const navShow = () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav-links');
+
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+        burger.classList.toggle('close');
+    });
+
+
+
+}
+navShow();
